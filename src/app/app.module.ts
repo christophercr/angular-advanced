6a7a2e2588f36fb67214497bdccab1e0598bd889
christophercr <christophercr@gmail.com>
2019-05-13 12:25:32 +0200

    Changes day 3: convert app into a PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {ApplicationRef, NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {ServiceWorkerModule, SwUpdate} from '@angular/service-worker';
 import {NgxsModule} from '@ngxs/store';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
+import {first} from 'rxjs/operators';
+import {concat, interval} from 'rxjs';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeModule} from './home/home.module';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,7 @@ import {HomeModule} from './home/home.module';
     BrowserModule,
     HomeModule,
     HttpClientModule,
-    NgxsModule.forRoot([], {developmentMode: true}),
+    NgxsModule.forRoot([], {developmentMode: !environment.production}),
     NgxsLoggerPluginModule.forRoot(),
     NgxsReduxDevtoolsPluginModule.forRoot({
       maxAge: 50, // retains last 50 states
@@ -26,6 +30,7 @@ import {HomeModule} from './home/home.module';
       disabled: false // disable this extension in production!
     }),
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -33,10 +38,41 @@ import {HomeModule} from './home/home.module';
 export class AppModule {
 
   // Diagnostic only: inspect router configuration
-  constructor(router: Router) {
+  constructor(router: Router, private swUpdate: SwUpdate, appRef: ApplicationRef) {
     // Use a custom replacer to display function names in the route configs
     // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
     //
     // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+
+    swUpdate.available.subscribe((event) => {
+      console.log('==========> New version available!');
+      console.log('==========> Current version is:', event.current);
+      console.log('==========> New available version is:', event.available);
+
+      const update = window.confirm('There is a new version of this app available. Do you want to update?');
+
+      if (update) {
+        this.swUpdate.activateUpdate().then(() => {
+          document.location.reload();
+          // console.log('---- The SW has activated the update');
+        });
+      }
+    });
+
+    swUpdate.activated.subscribe((event) => {
+      console.log('==========> The app has been updated to the latest version!');
+      console.log('==========> Old version was:', event.previous);
+      console.log('==========> New version is:', event.current);
+    });
+
+    const appStable$ = appRef.isStable.pipe(
+      first((isStable) => isStable === true) // emit only the first "true" value, then completes
+    );
+
+    const interval$ = interval(1000 * 10); // every 10 secs
+
+    concat(appStable$, interval$).subscribe((_value: any) => {
+      this.swUpdate.checkForUpdate().finally(() => console.log('-------- SW check for updates done!'));
+    });
   }
 }
